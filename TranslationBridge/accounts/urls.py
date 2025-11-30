f998d75bcd60e9a3ce1486f43a68a829374d344d
Hissah Saad <hissahsaad23@gmail.com>
2025-11-30 20:04:41 +0300

    signin - signup create page

--- TranslationBridge/accounts/urls.py
+++ TranslationBridge/accounts/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    
+    path('signup/', views.sign_up, name="sign_up"),
+    path('signin/', views.sign_in, name="sign_in"),
+    path('logout/', views.logout, name="logout"),
+    path('test/', views.test_view,name="test_view"),
+    path('profile/', views.profile_view, name="profile_view"),
+    path('update_profile/', views.update_profile_view, name="update_profile_view"),
 ]
--- TranslationBridge/accounts/views.py
+++ TranslationBridge/accounts/views.py
@@ -1,2 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 # Create your views here.
+
+#Sign up view
+def sign_up(request:HttpRequest):
+
+    if request.method == "POST":
+        try:
+            new_user = User.objects.create_user(username=request.POST["username"],password=request.POST["password"],email=request.POST["email"], first_name=request.POST["first_name"], last_name=request.POST["last_name"])
+            new_user.save()
+            messages.success(request,"Registered user successfuly", "alert-success")
+
+            return redirect("accounts:sign_in")
+        
+        except Exception as e:
+            print(e)
+    
+    return render(request, "accounts/signup.html")
+
+#Sign in view
+def sign_in(request:HttpRequest):
+
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        print(user)
+        if user:
+            login(request, user)
+            messages.success(request, "Logged in successfuly", "alert-success")
+            return redirect("accounts:test_view")
+        else:
+            messages.error(request," Please try again. You credentials are wrong", "alert-danger")
+
+    return render(request, "accounts/signin.html")
+
+
+#Logout view
+def logout (request:HttpRequest):
+
+    logout(request)
+    return redirect("accounts:test_view")
+
+    
+
+#test page
+def test_view(request:HttpRequest):
+
+    if request.user.is_authenticated:
+        print(request.user.email)
+    else:
+        print("user is not logged in")
+
+    return render(request, "accounts/test.html")
+
+#Profile view
+def profile_view(request:HttpRequest):
+
+    return render(request, "accounts/profile.html")
+
+#Profile update view
+def update_profile_view(request:HttpRequest):
+
+    return render(request, "accounts/update_profile.html")
