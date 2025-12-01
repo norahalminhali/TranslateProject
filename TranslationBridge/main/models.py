34f02229aa90630a766bafeeb4bc860e54ba0519
norah alminhali <norahmu515@gmail.com>
2025-12-01 08:26:45 +0300

    Update, Contact Page

--- TranslationBridge/main/models.py
+++ TranslationBridge/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Content(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
